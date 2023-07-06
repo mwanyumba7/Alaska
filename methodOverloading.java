ca07ef6f6fb4636f702231c0072dc19fe404ebc3
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2023-07-06 02:53:29 +0000

    modify: made methodOverloadig more readable

--- methodOverloading.java
+++ methodOverloading.java
@@ -1,36 +1,45 @@
 import java.util.Scanner;
 
-class methodOverloading {
+public class methodOverloading {
     public static void main(String[] args) {
-        int a,b;
-        double c,d;
-        System.out.println("ENTER THE VALUES OF a AND b:\n");
-        Scanner in=new Scanner(System.in);
-        a=in.nextInt();
-        b=in.nextInt();
-        System.out.println("ENTER THE VALUES OF c AND d:\n");
-        Scanner inn=new Scanner(System.in);
-        c=inn.nextDouble();
-        d=inn.nextDouble();
-     int r1 = minFunction(a, b);
-     double r2 = minFunction(c,d);
-     System .out.println("Minimum Value = " + r1);
-     System .out.println("Minimum Value = " + r2);
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter two integers
+        System.out.println("Enter two integer values:");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        // Prompt the user to enter two doubles
+        System.out.println("Enter two double values:");
+        double c = scanner.nextDouble();
+        double d = scanner.nextDouble();
+
+        // Call the minFunction method with integer and double parameters
+        int minInt = minFunction(a, b);
+        double minDouble = minFunction(c, d);
+
+        // Print the minimum values returned by the minFunction method
+        System.out.println("Minimum value of integers: " + minInt);
+        System.out.println("Minimum value of doubles: " + minDouble);
     }
-     public static int minFunction(int n1, int n2) {
-     int min;
-     if (n1 > n2)
-     min = n2;
-     else
-     min = n1;
-     return min;
-    }
-    public static double minFunction(double n1, double n2){
-        double min;
-        if(n1>n2)
-        min=n2;
+
+    // Method to find the minimum of two integers
+    public static int minFunction(int n1, int n2) {
+        int min;
+        if (n1 > n2)
+            min = n2;
         else
-        min=n1;
+            min = n1;
         return min;
     }
-}
+
+    // Method to find the minimum of two doubles
+    public static double minFunction(double n1, double n2) {
+        double min;
+        if (n1 > n2)
+            min = n2;
+        else
+            min = n1;
+        return min;
+    }
+} 
